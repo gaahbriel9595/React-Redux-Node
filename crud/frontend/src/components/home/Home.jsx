d62d6d7248192fcc76d3ec818596d35b46604382
gaahbriel9595 <43424762+gaahbriel9595@users.noreply.github.com>
2019-08-06 03:22:04 -0300

    Add files via upload

---
+++ crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início" subtitle="Segundo Projeto do capítulo de React.">
+        <div className="display-4">Bem vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para exeplificar um cadastro de usuário desenvolvido em React!</p>
+    </Main>
---
+++ crud/frontend/src/components/template/Header.jsx
@@ -0,0 +1,7 @@
+import './Header.css'
+import React from 'react'
+
+export default pros =>
+    <header className="header">
+        Cabeçalho
+    </header>
