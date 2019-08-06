f515d6f8dbd9700a522192f58d211d955e4efbe1
gaahbriel9595 <43424762+gaahbriel9595@users.noreply.github.com>
2019-08-06 03:17:42 -0300

    Add files via upload

---
+++ BillingCycles/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+          <h1>Teste</h1>
+      </div>
+    </div>
+  )
+}
---
+++ BillingCycles/frontend/src/common/template/dependencies.js
@@ -0,0 +1,11 @@
+import 'modules/admin-lte/plugins/jQueryUI/jquery-ui.min'
+import 'modules/admin-lte/plugins/fastclick/fastclick'
+import 'modules/admin-lte/plugins/slimScrolls/jquery.slimscroll.min'
+import 'modules/admin-lte/dist/js/app.min'
+
+import 'modules/fonst-awesome/css/font-awesome.min.css'
+import 'modules/ionicons/dist/css/ionicons.min.css'
+import 'modules/admin-lte/bootstrap/css/bootstrap.min.css'
+import 'modules/admin-lte/dist/cssAdminLTE.min.css'
+import 'modules/admin-lte/dist/css/skins/_all-skins.min.css'
+import 'modules/admin-lte/plugins/iCheck/flat/blue.css'
